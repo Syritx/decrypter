ad90e3a7689b0c29560f80ca6029e291268306c4
Dmitri <56200546+Syritx@users.noreply.github.com>
2020-09-01 11:27:19 -0400

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -4,9 +4,6 @@ import hashlib
 from itertools import product
 from string import ascii_lowercase
 
-letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"
-          ,"0","1","2","3","4","5","6","7","8","9",".","-","_",":",";"]
-
 found_hash = None
 word = ""
 
