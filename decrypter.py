c1e7d19012139e10fce9246458bca8ebbe46e2ec
Dmitri <56200546+Syritx@users.noreply.github.com>
2020-09-01 14:53:32 -0400

    changed decryption

--- decrypter.py
+++ decrypter.py
@@ -1,5 +1,5 @@
 import time
-import hashlib
+import hashlib as h
 
 from itertools import product
 from string import ascii_lowercase
@@ -7,21 +7,24 @@ from string import ascii_lowercase
 found_hash = None
 word = ""
 
-def decrypt_password(hash, length):
+def decrypt_password(hash):
     global word, found_hash
-    runtime = 0
+    max_length = 20
 
     while True:
 
-        for c in product(ascii_lowercase,repeat=length):
+        for l in range(max_length):
+            for c in product(ascii_lowercase,repeat=l):
+                word = ''.join(c)
+                found_hash = h.md5(word).hexdigest()
 
-            word = ''.join(c)
-            found_hash = hashlib.md5(str.lower(word)).hexdigest()
+                if hash == found_hash:
+                    print(word + " is the password")
+                    found = True
+                    break
+                else: print("checking [" + word + "]")
 
-            print("checking... [" + str(word) + "] doesn't match")
-
-            if hash == found_hash:
-                print(word + " is the password \nRuntime: " + str(runtime))
+            if found:
                 break
-
         break
+
